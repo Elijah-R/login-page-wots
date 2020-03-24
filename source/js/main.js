00de04e13fc0ffe9d450ea7fa069a283b9cade0e
Elijah-R <sometry@gmail.com>
2020-03-24 23:33:40 +0300

    Initial commit

---
+++ source/js/main.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// import './general/polyfills';
+// import './general/intersection-observer';
+// import vhHeightCalc from './general/vh-height-calc';
+// import classAddRemoveDelay from './general/class-add-remove-delay';
+// import scaleCalc from './general/scale-calc';
+// import lazyload from './general/lazyload';
+/* eslint-enable */
+
+// region Blocks
+// import example from '../blocks/example/example';
+import formInputs from '../blocks/form-input/form-input';
+
+// endregion
+
+formInputs();
